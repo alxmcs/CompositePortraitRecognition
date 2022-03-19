444163871458a653ee519fcd6387daf9eda75cfd
Василий Родин <varodg2000@mail.ru>
2022-03-19 10:48:57 +0300

    issue#8 : обновил скрипт(он лежит в самой папке, я его в пакет не поместил) + табличку с результатами

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
+import openpyxl
+
 import utils.face_encoding
 import utils.style_transfer
+from openpyxl import load_workbook
 
 if __name__ == "__main__":
 
     transfer_model = utils.style_transfer.TransferModel(utils.style_transfer.MODEL_URL)
 
-    images = ["images\photos\photo1.png"]
+    images = ["images\photos\photo1.png",
+              "images\photos\photo2.png",
+              "images\photos\photo3.png",
+              "images\photos\photo4.png"]
 
-    sketches = ["images\sketches\sketch1.png"]
+    sketches = ["images\sketches\sketch1.png",
+                "images\sketches\sketch2.png",
+                "images\sketches\sketch3.png",
+                "images\sketches\sketch4.png"]
 
     # before style_transfer
     encodings_array_before = []
@@ -23,15 +32,26 @@ if __name__ == "__main__":
         image_with_style = transfer_model.process_image(path0, path1, path_image_with_style)
         new_distance = utils.face_encoding.calculate_distance(path_image_with_style, path1)
         encodings_array_after.append(new_distance)
-        # print("iteration number " + i.__str__())
+        print("iteration number " + i.__str__())
 
-    avg_before = sum(encodings_array_before)/len(encodings_array_before)
-    avg_after = sum(encodings_array_after)/len(encodings_array_after)
+    # эта часть кода считает средний выигрыш
+    # avg_before = sum(encodings_array_before)/len(encodings_array_before)
+    # avg_after = sum(encodings_array_after)/len(encodings_array_after)
+    #
+    # print("avg_before " + avg_before.__str__() + "\n" + "avg_after " + avg_after.__str__())
+    #
+    # # насколько стало лучше
+    # improve = avg_before/avg_after
+    # print("improve = " + improve.__str__())
 
-    print("avg_before " + avg_before.__str__() + "\n" + "avg_after " + avg_after.__str__())
+    headers = ['№ пары изображений', 'Расстояние до переноса стиля', 'Расстояние после переноса стиля']
 
-    # насколько стало лучше
-    improve = avg_before/avg_after
-    print("improve = " + improve.__str__())
+    book = openpyxl.Workbook()
+    sheet_1 = book.create_sheet("results", 0)
+    sheet_1.append(headers)
 
-    # если добавить 3 и 4 фотографии, то перенос стиля наоборот увеличит меру близости энкодингов изображений
+    for i in range(0, len(encodings_array_before)):
+        sheet_1.append([i + 1, encodings_array_before[i], encodings_array_after[i]])
+
+    book.save("results/results.xlsx")
+
