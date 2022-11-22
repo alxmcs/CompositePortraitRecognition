4149d6fb6b36498ef12c111639adc424b1ce655e
Василий Родин <varodg2000@mail.ru>
2022-11-22 18:26:27 +0400

    fixed the possibility of occurrence UnicodeDecodeError

--- db/database_init.py
+++ db/database_init.py
@@ -1,9 +1,9 @@
 import sqlite3
-
+import codecs
 conn = sqlite3.connect("database.db")
 cursor = conn.cursor()
 
-fd = open('mydb.sql', 'r')
+fd = open('mydb.sql', 'r', encoding='utf-8')
 sqlFile = fd.read()
 fd.close()
 
